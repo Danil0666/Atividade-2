0094e9553d9ab4ed4affd990215abb01964de801
Terano <102757870+Danil0666@users.noreply.github.com>
2022-05-12 15:36:54 -0300

    Add files via upload

---
+++ atividade12.js
@@ -0,0 +1,36 @@
+ 
+ //Ativ 1
+
+ let vot = parseInt(prompt('BORA VOTAR '));
+let NASC = parseInt(prompt('VEJA SE VOCÊ PODE VOTAR '));
+
+if( nasc <= 2006 ){
+    //console.log(`Voce pode Votar `)
+}else{
+   //console.log('Você Não pode Votar')
+
+//ATIV 2    
+
+let val = parseInt(prompt('SENHA DO USUARIO'));
+
+if( val == 1234 ){
+   //console.log(`Senha valida `)
+}else{
+   //console.log ('Senha Invalida')
+
+  //  }
+
+    //ATIV 3
+    let turno = prompt('em qual turno voce estuda?')
+
+    if (turno == 'm'){
+        console.log('bom dia')
+    }else if(turno == 'v'){
+        console.log('boa tarde')
+    }else if(turno == 'n'){
+        console.logrt('boa noite')
+    }else{
+        console.log('valor infalido')
+
+    }
+     
